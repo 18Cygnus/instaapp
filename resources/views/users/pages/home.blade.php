99a39988cc355de0e6d6fee26a6ef9a14dac6f37
18Cygnus <aryasanjaya001@gmail.com>
2025-07-02 20:13:30 +0700

    refactor: enhancing ui for home, post, and navbar

--- resources/views/users/pages/home.blade.php
+++ resources/views/users/pages/home.blade.php
@@ -1,37 +1,40 @@
 @extends('users.layout.user')
 
 @section('user')
-    <section class="flex flex-col w-full items-center pb-24">
-
-        @foreach ( $posts as $post )
-        
-        <div class="card-contents w-[400px] bg-gray-200 px-4 py-2 flex flex-col gap-4 border-b border-gray-300">
-            <div class="card-profile flex items-center justify-between gap-4">
-                <div class="left-profile flex items-center gap-4">
-                    <img src="{{ asset('img/profile.png') }}" alt="" class="w-10 rounded-full">
-                    <p>Arya</p>
-                </div>
-                <div class="right-profile">
-                    <i class="ri-more-fill"></i>
-                </div>
-            </div>
-            <div class="card-content">
-                <img src="{{ route('image.show', $post->image) }}" alt="" class="w-full rounded-xl">
-                <p class="mt-1">{{$post->text}}</p>
-            </div>
-            <div class="card-action flex items-center gap-3">
-                <div class="flex items-center gap-1">
-                    <i class="ri-heart-line"></i>
-                    <p>10</p>
-                </div>
-                <div class="flex items-center gap-1">
-                    <i class="ri-message-3-line"></i>
-                    <p>3</p>
-                </div>
-            </div>
+    <section class="flex flex-col w-full items-center bg-gray-200 pb-16">
+        <div class="flex items-center justify-between w-[500px] px-4 py-5 bg-white fixed">
+            <h1 class="text-2xl font-bold">Instagram</h1>
+            <i class="ri-chat-1-line text-2xl"></i>
         </div>
-
-        @endforeach
+        <div class="p-3 flex w-full flex-col gap-2 pt-20">
+            @foreach ( $posts as $post )
+            <div class="card-contents w-full bg-white flex flex-col rounded-xl pb-4">
+                <div class="card-profile px-4 py-3 flex items-center justify-between gap-4">
+                    <div class="left-profile flex items-center gap-4">
+                        <img src="{{ asset('img/profile.png') }}" alt="" class="w-10 rounded-full">
+                        <p>Arya</p>
+                    </div>
+                    <div class="right-profile">
+                        <i class="ri-more-fill"></i>
+                    </div>
+                </div>
+                <div class="card-content">
+                    <img src="{{ route('image.show', $post->image) }}" alt="" class="w-full">
+                    <p class="mt-1 px-4">{{$post->text}}</p>
+                </div>
+                <div class="card-action px-4 py-1 flex items-center gap-3">
+                    <div class="flex items-center gap-1">
+                        <i class="ri-heart-line"></i>
+                        <p>10</p>
+                    </div>
+                    <div class="flex items-center gap-1">
+                        <i class="ri-message-3-line"></i>
+                        <p>3</p>
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
 
     </section>
 @endsection
